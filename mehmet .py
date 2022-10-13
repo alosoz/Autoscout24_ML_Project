49e0df420f5feaea9971a46a73ab320e14ac555a
brhnstr <burhansatar01@gmail.com>
2022-10-13 15:23:48 +0200

    15.23 update

---
+++ mehmet .py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Wed May 07 12:00:00 2022
+@author: PyBoys
+"""
+
+#Import libraries
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import date
+current_year = date.today().year
+
+#load the model from disk
+import joblib
+filename = 'finalized_model.sav'
+model = joblib.load(filename)
+
+sc = joblib.load(open('scaler.sav', 'rb'))
+
+#Import python scripts
+from preprocessing import preprocess,make_model_dict, n_dict
+
+def main():
+    #Setting Application title
+    st.title('PyBoys AutoScout Model App')
+
+      #Setting Application description
+    st.markdown("""
+     :dart:  This Streamlit app is made to predict AutoScout use case.
+    The application is functional for online prediction. \n
+    """)
+    st.markdown("<h3></h3>", unsafe_allow_html=True)
+
+    #Setting Application sidebar default
+    add_selectbox = st.sidebar.selectbox(
+    "How would you like to predict?", ("Online", "Default"))
+    st.sidebar.info('This app is created to predict AutoScout use case')
+    
+
+
+    if add_selectbox == "Online":
+        st.info("Input data below")
+        #Based on our optimal features selection
+        st.subheader("The Most Important Features:")
+        make_model_label = st.selectbox('Make and Model of the Car' , (make_model_dict))
+        first_registration = st.number_input('First Registration Year of the Car', min_value=1940, max_value=2021, value=2000)
+        empty_weight = st.number_input('Empty Weight of the Car', min_value=0, value =0) #### ok
+        mileage = st.number_input('Mileage of the Car',value = 0)
+        Power = st.number_input('Power of the Car',value = 0)
+        fuel_consumption = st.number_input('Fuel Consumption of the Car',value = 0.0)
+        co2_emissions = st.number_input('Co2 emission per km', value = 0)
+        engine_size = st.number_input('Engine Size of the Car',value=0) 
+        gears = st.selectbox('Gear of the Car', ('1','2','3', '4','5','6','7', '8','9'))
+        seats = st.selectbox('Seat of the Car', ('1','2','3', '4','5','6','7', '8','9')) 
+        doors = st.selectbox('Door of the Car', ('1','2','3', '4','5','6','7', '8','9'))
+        cylinder = st.selectbox('Cylinder of the Car', ('1','2','3', '4','5','6','7', '8','9','10'))    
+        warranty_months = st.number_input('How many months does it have ?', value = 0)
+
+        age = current_year - first_registration 
+        total_consumption =  fuel_consumption * mileage/100 
+        total_co2_emissions = mileage * co2_emissions
+
+        data = {
+                'mileage': mileage, 
+                'seats': n_dict[seats],
+                'doors': n_dict[doors],
+                'warranty_months': warranty_months,
+                'Power': Power,
+                'engine_size': engine_size,
+                'Gears': n_dict[gears],
+                'Cylinders': n_dict[cylinder],
+                'Empty_weight':empty_weight,
+                'fuel_consumption': fuel_consumption,
+                'co2_emissions' : co2_emissions,
+                'first_registration_years': age,
+                'make_model_label': make_model_dict[make_model_label],# 1
+                'mileage_years': mileage/age,
+                'total_consumption': total_consumption, #
+                'total_co2_emissions': total_co2_emissions
+                }
+      #           'price', 'mileage', 'seats', 'doors', 'warranty_months', 'Power',
+      #  'engine_size', 'Gears', 'Cylinders', 'Empty_weight', 'fuel_consumption',
+      #  'co2_emissions', 'first_registration_years', 'make_model_label',
+      #  'mileage_years', 'total_consumption', 'total_co2_emissions'
+
+
+        
+        features_df = pd.DataFrame.from_dict([data])
+        st.markdown("<h3></h3>", unsafe_allow_html=True)
+        st.write('Overview of input is shown below')
+        st.markdown("<h3></h3>", unsafe_allow_html=True)
+        st.dataframe(features_df)
+
+
+        #Preprocess inputs
+        #preprocess_df = preprocess(features_df, 'Online')
+
+
+        features_df_scaled = sc.transform(features_df)
+
+        prediction = np.exp(model.predict(features_df_scaled))
+
+        if st.button('Predict'):
+            st.warning(prediction)
+        
+
+if __name__ == '__main__':
+        main()
--- web_deploy_autoscout24.py
+++ web_deploy_autoscout24.py
@@ -23,27 +23,27 @@ filename = 'RandomForestRegressor.sav'
 model = joblib.load(filename)
 
 #Import python scripts
-from preprocessing import preprocess
+from preprocessing_Elegant import preprocess, fuel_countryd, locationd, make_modeld, Gearboxd, Upholstery_Colord, upholsteryd, drivetraind, typed,body_typed, sellerd, fuel_typed, colourd, n_dictd
 
 def main():
     #Setting Application title
-    st.title('Fenyx Autoscout24 Model App')
+    st.title('Group Elegant Autoscout24 Model App')
 
       #Setting Application description
     st.markdown("""
-     :dart: Data for Autoscout24 \n
-     :dart:  Data for Autoscout24. \n
+     :dart:  This Streamlit app is made to predict AutoScout use case.
+    The application is functional for online prediction. \n
     """)
     st.markdown("<h3></h3>", unsafe_allow_html=True)
 
     #Setting Application sidebar default
-    colours_name = ['Please choose colour','Grey', 'White', 'Black', 'Red', 'Silver', 'Brown', 'Blue',
-       'Violet', 'Beige', 'Yellow', 'Green', 'Orange', 'Bronze', 'Gold']
-    drivetrain_names = ['Please choose type of drivetrain','Front', '4WD', 'Rear']
-    location_names = ['Please choose city','BREDA','ALMERE','UTRECHT','GRONINGEN','DEN BOSCH','AMSTERDAM','ROTTERDAM','HAARLEM','OSS',
- 'APELDOORN','ZWOLLE','TILBURG','EINDHOVEN','AMERSFOORT','LEEUWARDEN','ARNHEM','VENLO','ENSCHEDE','DEN HAAG','MAASTRICHT','DELFT','LEIDEN',]
-    fuel_types = ['Please choose fuel type','Gasoline', 'Diesel', 'Electric/Diesel', 'LPG', 'Electric','Electric/Gasoline', 'CNG', 'Ethanol', 'Others', 'Hydrogen']
-    upholstery_names = ['Please choose upholstery type','Cloth', 'Partleather', 'Fullleather', 'alcantara', 'Other','Velour']
+#     colours_name = ['Please choose colour','Grey', 'White', 'Black', 'Red', 'Silver', 'Brown', 'Blue',
+#        'Violet', 'Beige', 'Yellow', 'Green', 'Orange', 'Bronze', 'Gold']
+#     drivetrain_names = ['Please choose type of drivetrain','Front', '4WD', 'Rear']
+#     location_names = ['Please choose city','BREDA','ALMERE','UTRECHT','GRONINGEN','DEN BOSCH','AMSTERDAM','ROTTERDAM','HAARLEM','OSS',
+#  'APELDOORN','ZWOLLE','TILBURG','EINDHOVEN','AMERSFOORT','LEEUWARDEN','ARNHEM','VENLO','ENSCHEDE','DEN HAAG','MAASTRICHT','DELFT','LEIDEN',]
+#     fuel_types = ['Please choose fuel type','Gasoline', 'Diesel', 'Electric/Diesel', 'LPG', 'Electric','Electric/Gasoline', 'CNG', 'Ethanol', 'Others', 'Hydrogen']
+#     upholstery_names = ['Please choose upholstery type','Cloth', 'Partleather', 'Fullleather', 'alcantara', 'Other','Velour']
     
     
     
@@ -55,45 +55,43 @@ def main():
     st.sidebar.image(image)
     st.sidebar.info('This app uses Random Forest Model')
     st.sidebar.image(image1)
+
+
+
     if add_selectbox == "Online":
         st.info("Input data below")
-        st.subheader("Initials of Vehicle data")
+        st.subheader("The Most Important Features of Model:")
         #Based on our optimal features selection
-        location = st.selectbox('Location', location_names)
-        First_Registration_Year = st.slider('First Registration Year' ,0,2022,1980)
-        Power_kW = st.slider('Power kW', 0,220,0)
-        Empty_Weight_kg = st.slider('Empty Weight (kg)', 0,5000,300)
-        mileage = st.slider('Mileage of Vehicle (km) ', 0,300000,1000)
-        gears = st.slider('Gears', 0,9,0)
-        make_model = st.slider('Make Model',0,800,0)
-        cylinders = st.slider('Cylinders', 0,12,0)
-        Engine_Size_cc = st.slider('Engine Size (cc)', 0,3000,600)
-        Gearbox = st.slider('Gearbox', 0,2,0)
-        fuel_type = st.selectbox('Fuel Type', fuel_types)
-        fuel_country = st.slider('Fuel Country', 0,7,0)
-        fuel_city = st.slider('Fuel City', 0,16,0)
-        fuel_comb = st.slider('Fuel Comb', 0,10,0)
-        co2_emissions = st.slider('CO2 Emissions', 0,300,0)
-        body_type = st.slider('Body Type', 0,5,0)
-        seats = st.slider('Count of Seats', 0,9,0)
-        doors = st.slider('Count of Doors', 0,6,0)
-        colour = st.selectbox('Colour', colours_name)
-        upholstery = st.selectbox('Upholstery', upholstery_names)
-        drivetrain = st.selectbox('Drivetrain', drivetrain_names)
+        location = st.selectbox('Location of the Auto' , (locationd))
+        First_Registration_Year = st.slider('First Registration Year' ,1980,2022,2020)     #st.number_input('First Registration Year of the Car', min_value=1940, max_value=2022, value=2000)
+        Power_kW = st.slider('Power kW', 0,220,100)
+        Empty_Weight_kg = st.slider('Empty Weight (kg)', 300,5000,2000)
+        mileage = st.slider('Mileage of Vehicle (km) ', 1000,300000,100000)
+        gears = st.selectbox('Gear of the Car', ('1','2','3', '4','5','6','7', '8','9'))
+        make_model = st.selectbox('Make and Model of the Auto' , (make_modeld))
+        cylinders = st.selectbox('Cylinder of the Car', ('1','2','3', '4','5','6','7', '8','9','10'))
+        Engine_Size_cc = st.slider('Engine Size (cc)', 600,3000,1400)
+        Gearbox = st.selectbox('Gearbox', (Gearboxd))
+        fuel_type = st.selectbox('Fuel Type', (fuel_typed))
+        fuel_country = st.selectbox('Fuel Country', (fuel_countryd))
+        fuel_city = st.slider('Fuel City', 0,16,10)
+        fuel_comb = st.slider('Fuel Comb', 0,10,5)
+        co2_emissions = st.slider('CO2 Emission per km', 0,300,90)
+        body_type = st.selectbox('Body Type', (body_typed))
+        seats = st.selectbox('Seat of the Car', ('1','2','3', '4','5','6','7', '8','9'))
+        doors = st.selectbox('Door of the Car', ('1','2','3', '4','5','6','7', '8','9'))
+        colour = st.selectbox('Colour', (colourd))
+        upholstery = st.selectbox('Upholstery', (upholsteryd))
+        drivetrain = st.selectbox('Drivetrain', (drivetraind))
 
-     
+        
         
 
-        st.subheader("Details of Vehicle Data")                       
-        title_0 = st.selectbox('LED Headlights:', ('No','Yes'))
-        title_1 = st.selectbox('Digital cockpit:', ('No','Yes'))
-        title_2 = st.selectbox('Heated streering weel:', ('No','Yes'))
-        title_3 = st.selectbox('Panorama roof:', ('No','Yes'))
-        title_4 = st.selectbox('Electrically adjustable seats:', ('No','Yes'))
-        title_5 = st.selectbox('Emergency System:', ('No','Yes')) 
-        title_6 = st.selectbox('Electric tailgate:', ('No','Yes'))
-        title_7 = st.selectbox('High bean assist:', ('No','Yes'))  
-        title_8 = st.selectbox('Full Service History:', ('No','Yes'))
+        # st.subheader("Details of Vehicle Data")                       
+        # title_0 = st.selectbox('LED Headlights:', ('No','Yes'))
+        # title_1 = st.selectbox('Digital cockpit:', ('No','Yes'))
+        # title_2 = st.selectbox('Panorama roof:', ('No','Yes'))
+        # title_3 = st.selectbox('Full Service History:', ('No','Yes'))
 
 
 
@@ -104,9 +102,9 @@ def main():
                 'Power kW' : Power_kW,
                 'Empty Weight (kg)': Empty_Weight_kg,
                 'Mileage of Vehicle (km)': mileage,
-                'Gears': gears,
+                'Gears': n_dictd[gears],
                 'Make Model': make_model,
-                'Cylinders': cylinders,
+                'Cylinders': n_dictd[cylinders],
                 'Engine Size (cc)': Engine_Size_cc,
                 'Gearbox': Gearbox,
                 'Fuel Type': fuel_type,
@@ -115,22 +113,17 @@ def main():
                 'Fuel Comb' : fuel_comb,
                 'CO2 Emissions': co2_emissions,
                 'Body Type' : body_type,
-                'Count of Seats': seats,
-                'Count of Doors': doors,
+                'Count of Seats':  n_dictd[seats],
+                'Count of Doors': n_dictd[doors],
                 'Colour': colour,
                 'Upholstery': upholstery,
                 'Drivetrain' : drivetrain,
                 
-                'title_0':title_0, 
-                'title_1':title_1, 
-                'title_2':title_2, 
-                'title_3':title_3, 
-                'title_4':title_4,
-                'title_5':title_5,
-                'title_6':title_6, 
-                'title_7':title_7, 
-                'title_8':title_8, 
-                                
+                # 'title_0':title_0, 
+                # 'title_1':title_1, 
+                # 'title_2':title_2, 
+                # 'title_3':title_3, 
+
                 }
 
         features_df = pd.DataFrame.from_dict([data])
@@ -153,23 +146,23 @@ def main():
 
     else:
         st.subheader("Dataset upload")
-        uploaded_file = st.file_uploader("Choose a file")
-        if uploaded_file is not None:
-            data = pd.read_csv(uploaded_file,encoding= 'utf-8')
-            #Get overview of data
-            st.write(data.head())
-            st.markdown("<h3></h3>", unsafe_allow_html=True)
-            #Preprocess inputs
-            preprocess_df = preprocess(data, "Batch")
-            if st.button('Predict'):
-               #Get batch prediction
-                prediction = model.predict(preprocess_df)
-                prediction_df = pd.DataFrame(prediction, columns=["Predictions"])
-                prediction_df = prediction_df.replace({1:'Yes, the passenger survive.', 0:'No, the passenger died'})
+        # uploaded_file = st.file_uploader("Choose a file")
+        # if uploaded_file is not None:
+        #     data = pd.read_csv(uploaded_file,encoding= 'utf-8')
+        #     #Get overview of data
+        #     st.write(data.head())
+        #     st.markdown("<h3></h3>", unsafe_allow_html=True)
+        #     #Preprocess inputs
+        #     preprocess_df = preprocess(data, "Batch")
+        #     if st.button('Predict'):
+        #        #Get batch prediction
+        #         prediction = model.predict(preprocess_df)
+        #         prediction_df = pd.DataFrame(prediction, columns=["Predictions"])
+        #         prediction_df = prediction_df.replace({1:'Yes, the passenger survive.', 0:'No, the passenger died'})
 
-                st.markdown("<h3></h3>", unsafe_allow_html=True)
-                st.subheader('Prediction')
-                st.write(prediction_df)
+        #         st.markdown("<h3></h3>", unsafe_allow_html=True)
+        #         st.subheader('Prediction')
+        #         st.write(prediction_df)
             
 if __name__ == '__main__':
         main()
